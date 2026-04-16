c377f23109f8c847823019ba8a1bdb3872f82353
admin <admin@admins-MacBook-Pro.local>
2026-04-16 08:17:07 +0300

    add case with slow outgoing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
@@ -16,5 +17,19 @@ class HomeController extends Controller
         sleep(2);
 
         return response('OK', 200);
+    }
+
+    public function slowRequest(): Response
+    {
+        sleep(2);
+
+        return response('OK', 200);
+    }
+
+    public function slow_outgoing(): Response
+    {
+        $response = Http::get('http://127.0.0.1/slow-request');
+
+        return response($response->body(), $response->status());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ Route::view('/', 'welcome')->name('home');
 
 Route::get('/fast', [HomeController::class, 'fast'])->name('fast');
 Route::get('/slow', [HomeController::class, 'slow'])->name('slow');
+Route::get('/slow-request', [HomeController::class, 'slowRequest'])->name('slow-request');
+Route::get('/slow-outgoing', [HomeController::class, 'slow_outgoing'])->name('slow-outgoing');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
